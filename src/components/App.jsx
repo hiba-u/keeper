1d0d141ea2c9935239d506587d5a48594005592f
hiba-u <coder.hiba@gmail.com>
2024-10-25 21:39:05 +0530

    create note

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
+import CreateArea from './CreateArea'
 import Note from "./Note"
 import Footer from "./Footer"
 import notes from './../notes'
 
 
 const App = () => {
+    const [notes, setNotes] = useState([])
+
+    const addNote = (note) => {
+        setNotes((prevState) => {
+            return [...prevState, note]
+        })
+    }
+
     return(
         <>
             <Header />
+            <CreateArea addNote={addNote}/>
             {notes.map((note) => {
                 return (
                     <Note key={note.id} title={note.title} content={note.content}/>
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+
+  const [note, setNote] = useState({})
+
+  function handleChange(e){
+    const {name, value} = e.target;
+
+    setNote((prevState) => {
+      return {...prevState, [name]: value}
+    })
+  }
+
+  return (
+    <div>
+      <form>
+        <input onChange={handleChange} name="title" placeholder="Title" value={note.title} />
+        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={note.content}/>
+        <button onClick={(e) => {
+          props.addNote(note)
+          e.preventDefault();
+          setNote({title:"", content: ""})
+        }}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
